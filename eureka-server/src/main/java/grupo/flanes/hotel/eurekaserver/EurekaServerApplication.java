99a01e5018a62c8891597a3e9304618a869c17e3
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-07 17:18:52 -0400

    chore: inicializacion del monorepo con eureka, ms-auth y ms-reserva

---
+++ eureka-server/src/main/java/grupo/flanes/hotel/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package grupo.flanes.hotel.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
---
+++ eureka-server/src/test/java/grupo/flanes/hotel/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo.flanes.hotel.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
