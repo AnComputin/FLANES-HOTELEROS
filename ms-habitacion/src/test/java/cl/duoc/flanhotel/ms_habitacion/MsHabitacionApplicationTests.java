baf62fbdebf7558d82253d0388cb74afd692c986
Edelgard777 <markmars17@gmail.com>
2026-05-13 13:46:58 -0400

    refactor: configuracion limpia desde cero en casa sin eureka

---
+++ ms-habitacion/src/test/java/cl/duoc/flanhotel/ms_habitacion/MsHabitacionApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.flanhotel.ms_habitacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHabitacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
