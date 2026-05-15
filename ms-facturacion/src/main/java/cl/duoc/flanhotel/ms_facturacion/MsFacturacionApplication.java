7c89d2fcc9534b42194b949ffbb37ccfbbd6aac0
AnComputin <an.riquelmez@duocuc.cl>
2026-05-15 14:47:37 -0400

    feat: subiendo ms-facturacion

---
+++ ms-facturacion/src/main/java/cl/duoc/flanhotel/ms_facturacion/MsFacturacionApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.flanhotel.ms_facturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFacturacionApplication.class, args);
+	}
+
+}
